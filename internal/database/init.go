b6e6077792a47a23002cdbe01db02232ac67f438
sanjaysagar12 <sanjaysagar.main@gmail.com>
2025-02-01 07:12:14 +0000

    Refactor database initialization code for improved readability

--- internal/database/init.go
+++ internal/database/init.go
@@ -1,43 +1,43 @@
 package database
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "os"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func Init() {
-    db_path, _ := GetPath()
-    db, err := sql.Open("sqlite3", db_path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	db_path, _ := GetPath()
+	db, err := sql.Open("sqlite3", db_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    // Enable foreign key constraints
-    _, err = db.Exec("PRAGMA foreign_keys = ON;")
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Enable foreign key constraints
+	_, err = db.Exec("PRAGMA foreign_keys = ON;")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Create the workspace table
-    createWorkspaceTableSQL := `CREATE TABLE IF NOT EXISTS workspace (
+	// Create the workspace table
+	createWorkspaceTableSQL := `CREATE TABLE IF NOT EXISTS workspace (
         "id" INTEGER PRIMARY KEY AUTOINCREMENT,
         "name" TEXT UNIQUE,
         "desc" TEXT,
         "total_containers" INTEGER DEFAULT 0,
         "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP
     );`
-    _, err = db.Exec(createWorkspaceTableSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = db.Exec(createWorkspaceTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Create the container table
-    createContainerTableSQL := `CREATE TABLE IF NOT EXISTS container (
+	// Create the container table
+	createContainerTableSQL := `CREATE TABLE IF NOT EXISTS container (
         "id" INTEGER PRIMARY KEY AUTOINCREMENT,
         "workspace_id" INTEGER,
         "name" TEXT,
@@ -51,18 +51,18 @@ func Init() {
         "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY(workspace_id) REFERENCES workspace(id) ON DELETE CASCADE
     );`
-    _, err = db.Exec(createContainerTableSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = db.Exec(createContainerTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Println("Tables created successfully!")
+	log.Println("Tables created successfully!")
 }
 
 func GetPath() (string, error) {
-    ex, err := os.Executable()
-    if err != nil {
-        return "", fmt.Errorf("failed to get executable path: %v", err)
-    }
-    return fmt.Sprintf("%s.db", ex), nil
+	ex, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("failed to get executable path: %v", err)
+	}
+	return fmt.Sprintf("%s.db", ex), nil
 }
